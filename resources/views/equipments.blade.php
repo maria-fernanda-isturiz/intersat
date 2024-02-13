0964ab59824cec43dfaa8ee912c15cea509a06cf
Maria Fernanda Isturiz <mfibarajas@gmail.com>
2024-02-13 01:07:06 -0600

    Update equipments.blade.php

--- resources/views/equipments.blade.php
+++ resources/views/equipments.blade.php
@@ -10,7 +10,11 @@
     <h1 class="mt-3 text-center">Nuestros equipos disponibles</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <div class="col text-center mb-3">
-      <a href="{{route('register_equipments')}}" class="btn btn-success">Agregar nuevos equipos</a>
+<<<<<<< Updated upstream
+      <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalAddEquipment">Agregar equipos nuevos</button>
+=======
+      <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalAddEquipment">Agregar Equipo Nuevo</button>
+>>>>>>> Stashed changes
     </div>
     <table class="table table-hover">
         <thead>
@@ -35,6 +39,42 @@
               <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#ModalDeleteEquipment{{$equipment->id}}">Eliminar</button>
             </td>
           </tr>
+
+          <div class="modal" id="ModalAddEquipment" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+<<<<<<< Updated upstream
+                  <h5 class="modal-title">Agregar Equipo</h5>
+=======
+                  <h5 class="modal-title">Agregar Equipo Nuevo</h5>
+>>>>>>> Stashed changes
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="{{route('new_equipments')}}" method="post" enctype="multipart/form-data">
+                  @csrf
+                <div class="modal-body">
+                  <div class="mb-3">
+                    <label for="" class="form-label">Nombre del equipo:</label>
+                    <input type="text" class="form-control" name="equipment" id="" placeholder="Ingrese el nombre del equipo nuevo a agregar" value="">
+                  </div>
+                  <div class="mb-3">
+                    <label for="" class="form-label">Descripción del equipo y sus características:</label>
+<<<<<<< Updated upstream
+                    <textarea class="form-control" id="" name="description" rows="3" placeholder="Ingrese una descripción nueva del equipo a actualizar" value=""></textarea>
+=======
+                    <textarea class="form-control" id="" name="description" rows="3" placeholder="Ingrese una descripción del equipo a agregar" value=""></textarea>
+>>>>>>> Stashed changes
+                  </div>                  
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                  <input type="submit" class="btn btn-success" value="Agregar Equipo Nuevo">
+                </div>
+              </form>
+              </div>
+            </div>
+          </div>
 
           <div class="modal" id="ModalEquipment{{$equipment->id}}" tabindex="-1">
             <div class="modal-dialog">
@@ -80,7 +120,7 @@
                 <div class="modal-body">
                   <input type="hidden" name="id_equipment" value="{{$equipment->id}}">
                   <div class="mb-3">
-                    <label for="" class="form-label">¿Desea eliminar el equipo y dejar el dispositivo fuera de servicio?</label>
+                    <label for="" class="form-label">¿Desea eliminar el equipo y dejar el dispositivo fuera de servicio? Cambie el estado de disponible a inactivo</label>
                     <select name="status" id="" class="form-control">
                     @foreach ($equipment_status as $status)
                         <option value="{{$status->id}}">{{$status->status}}</option>
